f2006aba468d185841e4cdeb081f3c91190ebbb6
sam <1002064@gmx.com>
2016-10-26 20:23:39 +0100

    Finished basic crud user interface for articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 	def index
+		@articles = Article.all
 	end
 
 	def new
@@ -15,6 +16,28 @@ class ArticlesController < ApplicationController
 			flash.now[:alert] = "Article has not been published."
 			render "new"
 		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article, notice: "Article has been edited."
+		else
+			flash.now[:alert] = "Article has not been edited."
+			render "edit"
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path, notice: "Article has been deleted."
 	end
 
 	def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
 	validates :name, presence: true
+	validates :content, presence: true, length: { minimum: 10 }
 end
