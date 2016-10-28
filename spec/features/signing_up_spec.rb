b34cff867bd1a2d7621b38528d55f9aa04e8add2
sam <1002064@gmx.com>
2016-10-28 16:31:23 +0100

    Can sign-up with email via devise gem

---
+++ spec/features/signing_up_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Users are able to sign up" do
+	before do
+		visit "/"
+	end
+
+	scenario "with valid details" do
+		click_link "Sign Up"
+		fill_in "Email", with: "john@test.com"
+		fill_in "user_password", with: "password"
+		fill_in "Password confirmation", with: "password"
+		click_button "Sign up"
+
+		expect(page).to have_content("You have signed up successfully.")
+	end
+end
+
