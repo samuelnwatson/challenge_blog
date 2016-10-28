b4499e2401738c04eb1dfd9db66d34a3c929bc2a
sam <1002064@gmx.com>
2016-10-28 16:59:55 +0100

    Users can now sign out.

---
+++ spec/features/signing_out_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "Users can sign out" do
+	let!(:user) { FactoryGirl.create(:user) }
+
+	before do
+		login_as(user)
+
+		visit "/"
+	end
+
+	scenario "sign out" do
+		click_link "Sign Out"
+
+		expect(page).to have_content "Signed out successfully."
+	end
+end
