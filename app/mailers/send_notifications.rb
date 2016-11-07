a0644a7d5615a9c575348b4f961b3f5c604db76b
sam <1002064@gmx.com>
2016-11-07 13:21:18 +0000

    Reduced repetition in SendNotifications

--- app/mailers/send_notifications.rb
+++ app/mailers/send_notifications.rb
@@ -1,15 +1,12 @@
 class SendNotifications < ApplicationMailer
+    @email = email
+    @subject = article.name
+
 	def later(email, article)
-	    @email = email
-	    @subject = article.name
-		@url  = "http://challengeblog.com"
 		mail(to: @email, subject: @subject).deliver_later
 	end
 
 	def now(email, article)
-	    @email = email
-	    @subject = article.name
-		@url  = "http://challengeblog.com"
 		mail(to: @email, subject: @subject).deliver_now
 	end
 end
