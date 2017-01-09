7330382e48ea7681b2ace398b42036b3efb8fd1e
sam <1002064@gmx.com>
2017-01-09 17:44:05 +0000

    Fixing mail error shown in heroku log

--- app/mailers/send_notifications.rb
+++ app/mailers/send_notifications.rb
@@ -1,12 +1,10 @@
 class SendNotifications < ApplicationMailer
-  @email = email
-  @subject = article.name
 
   def later(email, article)
-    mail(to: @email, subject: @subject).deliver_later
+    mail(to: email, subject: article.name).deliver_later
   end
 
   def now(email, article)
-    mail(to: @email, subject: @subject).deliver_now
+    mail(to: email, subject: article.name).deliver_now
   end
 end
